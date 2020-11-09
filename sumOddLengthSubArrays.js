c3e576949656aff8f46122c439b07f14ee13838c
https://learn.co/christa-gammage <crgammage@gmail.com>
2020-11-09 14:40:02 -0500

    odd length subarray sums

---
+++ sumOddLengthSubArrays.js
@@ -0,0 +1,14 @@
+var sumOddLengthSubarrays = function(arr, sum=0) {
+    const n = arr.length;
+     
+     //Generate the prefix sum array
+     const prefix = Array(n+1).fill(0);
+     for (let i = 0; i < n; i++)
+         prefix[i+1] = prefix[i]+arr[i];
+     
+     //Loop through all odd-length sequences and add sums to answer, then return
+     for (let i = 0; i < n; i++)
+         for (let j = i+1; j < n+1; j+=2)
+             sum += (prefix[j]-prefix[i]);
+     return sum;
+ };
