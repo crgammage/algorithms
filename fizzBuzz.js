0d1998536c07de042b482c35c8e58aa70b67dcf6
https://learn.co/christa-gammage <crgammage@gmail.com>
2020-11-13 14:05:07 -0500

    alternative solution

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -5,4 +5,13 @@ function fizzBuzz(n) {
     else if (i % 5 == 0) console.log("Buzz");
     else console.log(i);
 }
+
+function fizzBuzz(n) {
+    for (let i = 0; i < n; i++) {
+        if (i % 15 === 0) console.log("FizzBuzz")
+        else if (i % 3 === 0) console.log("Fizz")
+        else if (i % 5 === 0) console.log("Buzz")
+        else console.log(i)
+    }
+}
 }
